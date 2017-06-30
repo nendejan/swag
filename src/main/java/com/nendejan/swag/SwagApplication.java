48982b7def35d562eae87f9d375fdaaa2b973714
nico <nendejan@gmail.com>
2017-06-30 13:22:48 -0500

    initial commit

---
+++ src/main/java/com/nendejan/swag/SwagApplication.java
@@ -0,0 +1,12 @@
+package com.nendejan.swag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwagApplication.class, args);
+	}
+}
