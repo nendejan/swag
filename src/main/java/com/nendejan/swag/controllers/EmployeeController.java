e88b9a0aa18e43457783dd5d91e4400dfdeac6b1
nico <nendejan@gmail.com>
2017-06-30 15:19:06 -0500

    Started working on employee controller.

--- src/main/java/com/nendejan/swag/controllers/EmployeeController.java
+++ src/main/java/com/nendejan/swag/controllers/EmployeeController.java
@@ -1,13 +1,54 @@
 package com.nendejan.swag.controllers;
 
+import com.nendejan.swag.models.Employee;
+import com.nendejan.swag.models.data.DayDao;
+import com.nendejan.swag.models.data.EmployeeDao;
+import com.nendejan.swag.models.data.ScheduleDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by nico on 6/30/2017.
  */
 
 @Controller
+@RequestMapping("employee")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    private ScheduleDao scheduleDao;
+
+    @Autowired
+    private DayDao dayDao;
+
+    @RequestMapping(value="")
+    public String index(Model model) {
+
+        model.addAttribute("employees", employeeDao.findAll());
+        model.addAttribute("title", "Employee List");
+
+        return "employee/index";
+    }
+
+    @RequestMapping(value="add", method= RequestMethod.GET)
+    public String displayAddEmployeeForm(Model model){
+
+        model.addAttribute("title", "Add Employee");
+        model.addAttribute(new Employee());
+
+        //TODO Stopped here.
+        return "employee/add";
+    }
+
+
+
+
 
 
 }
--- src/main/java/com/nendejan/swag/models/data/EmployeeDao.java
+++ src/main/java/com/nendejan/swag/models/data/EmployeeDao.java
@@ -1,7 +1,17 @@
 package com.nendejan.swag.models.data;
 
+import com.nendejan.swag.models.Employee;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
 /**
  * Created by nico on 6/30/2017.
  */
-public interface EmployeeDao {
+@Repository
+@Transactional
+public interface EmployeeDao extends CrudRepository<Employee, Integer>{
+
 }
